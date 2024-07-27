4a794449a9d15eecdb7916a4201cc92a11ae9b6b
Ali Helmi <ahelmi365@gmail.com>
2024-07-27 16:19:12 +0300

    add router.go()

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 import Store from "./services/Store.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 document.addEventListener("DOMContentLoaded", async (event) => {
   console.log("DOM-Content Loaded!");
   await loadData();
   console.log(app.store.menue);
+
+  app.router.init();
+  app.router.go("/");
 });
