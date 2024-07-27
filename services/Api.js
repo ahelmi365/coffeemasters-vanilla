4165d765d1f56697830c0c2dd49dce9efd8f78fe
Ali Helmi <ahelmi365@gmail.com>
2024-07-27 14:30:31 +0300

    use api.url

--- services/Api.js
+++ services/Api.js
@@ -2,7 +2,7 @@ const API = {
   url: "../data/menu.json",
   fetchMenu: async () => {
     try {
-      const res = await fetch(this.url);
+      const res = await fetch(API.url);
       return await res.json();
     } catch (error) {
       console.log({ error });
