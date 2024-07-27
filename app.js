fde641c76c984a487ad9fa5465fe0f06ff4735c7
Ali Helmi <ahelmi365@gmail.com>
2024-07-27 14:29:48 +0300

    defetch data from api

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+import API from "./services/API.js";
+import Store from "./services/Store.js";
+
+window.app = {};
+app.store = Store;
+
+document.addEventListener("DOMContentLoaded", async (event) => {
   console.log("DOM-Content Loaded!");
+
+  app.store.menue = await API.fetchMenu();
+
+  console.log(app.store.menue);
 });
