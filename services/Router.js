4016e4711b814a418dd519595993f14d8df29b33
Ali Helmi <ahelmi365@gmail.com>
2024-07-27 16:18:47 +0300

    implment routing

---
+++ services/Router.js
@@ -0,0 +1,57 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        const route = event.currentTarget.getAttribute("href");
+        Router.go(route);
+      });
+    });
+
+    // Event handler for URL change
+    window.addEventListener("popstate", (event) => {
+      console.log(event.state);
+      Router.go(event.state.route, false);
+    });
+
+    // check the inital url
+    Router.go(location.pathname);
+  },
+
+  go: (route, addTowHistory = true) => {
+    console.log(`Going to ${route}`);
+    if (addTowHistory) {
+      history.pushState({ route }, "", route);
+    }
+
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Order";
+        break;
+
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          const productId = route.split("-")[1];
+          pageElement.dataset.id = productId;
+          pageElement.textContent = "Details product";
+        }
+        break;
+    }
+    const mainElement = document.querySelector("main");
+
+    mainElement.innerHTML = "";
+    mainElement.append(pageElement);
+    window.scrollX = 0;
+    window.scrollY = 0;
+  },
+};
+
+export default Router;
