732fbf61fad252f51d2281a16fb68fa05ccec675
Ali Helmi <ahelmi365@gmail.com>
2024-07-28 15:45:52 +0300

    import components and add event listener

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
 import Store from "./services/Store.js";
+// Link my Web Components
+import { MenuPage } from "./components/MenuPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+import { ProductItem } from "./components/ProductItem.js";
+import { CartItem } from "./components/CartItem.js";
 
 window.app = {};
 app.store = Store;
@@ -8,9 +14,16 @@ app.router = Router;
 
 document.addEventListener("DOMContentLoaded", async (event) => {
   console.log("DOM-Content Loaded!");
-  await loadData();
-  console.log(app.store.menue);
-
   app.router.init();
-  app.router.go("/");
+  await loadData();
+  console.log(app.store.menu);
+
+  // app.router.go("/");
+});
+
+window.addEventListener("appcartchange", (event) => {
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  const badge = document.getElementById("badge");
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
