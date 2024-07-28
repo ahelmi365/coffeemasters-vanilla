61f556525a3b8c016c3cef1e893e230b18aa3431
Ali Helmi <ahelmi365@gmail.com>
2024-07-28 15:47:22 +0300

    fix type

--- services/Menu.js
+++ services/Menu.js
@@ -1,5 +1,20 @@
 import API from "./API.js";
 
 export async function loadData() {
-  app.store.menue = await API.fetchMenu();
+  app.store.menu = await API.fetchMenu();
+}
+
+export async function getProductById(id) {
+  if (app.store.menu == null) {
+    await loadData();
+  }
+
+  for (let category of app.store.menu) {
+    for (let product of category.products) {
+      if (product.id == id) {
+        return product;
+      }
+    }
+  }
+  return null;
 }
