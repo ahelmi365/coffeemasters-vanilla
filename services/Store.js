a940636d25511fce6c7e707f3cd609e5fc4dbd6a
Ali Helmi <ahelmi365@gmail.com>
2024-07-28 15:48:03 +0300

    apply proxy to store obj

--- services/Store.js
+++ services/Store.js
@@ -1,6 +1,19 @@
 const Store = {
-  menue: null,
+  menu: null,
   cart: [],
 };
 
-export default Store;
+const proxiedStore = new Proxy(Store, {
+  set(target, property, value) {
+    target[property] = value;
+    if (property === "menu") {
+      console.log("menu is chnaged");
+      window.dispatchEvent(new Event("appmenuchange"));
+    } else if (property === "cart") {
+      console.log("Card is changed");
+      window.dispatchEvent(new Event("appcartchange"));
+    }
+    return true;
+  },
+});
+export default proxiedStore;
